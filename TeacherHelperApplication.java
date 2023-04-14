21d436ee308082ce5532d2d16dbab9a29e197b0f
johnADu <921337683@qq.com>
2023-04-14 20:39:19 +0800

    Add files via upload

---
+++ TeacherHelperApplication.java
@@ -0,0 +1,13 @@
+package com.coach.teacherHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherHelperApplication.class, args);
+	}
+
+}
